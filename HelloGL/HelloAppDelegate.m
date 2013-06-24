6fa0213467b112e223a0b2bb2218eb86bfc3d7e8
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-06-23 22:19:19 -0400

    Dev: screen draws an orange colour

--- HelloGL/HelloAppDelegate.m
+++ HelloGL/HelloAppDelegate.m
@@ -8,42 +8,35 @@
 
 #import "HelloAppDelegate.h"
 
+@interface HelloAppDelegate() <GLKViewDelegate>
+
+@end
+
 @implementation HelloAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    // create an EAGLContext, contains all the info needed for iOS to draw using OpenGL
+    EAGLContext *context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    // create a GLKView
+    GLKView *view = [[GLKView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // set the view to opaque to increase performance
+    [view setOpaque:YES];
+    // set the view's context and delegate
+    [view setContext:context];
+    [view setDelegate:self];
+    // add the view to the window
+    [self.window addSubview:view];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(0.898, 0.435, 0.239, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 @end
