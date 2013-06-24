33004b47ba8d31e38104e3f8e4e60e8865abab5d
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-06-24 01:12:40 -0400

    Dev: a rotating square now draws

--- HelloGL/HelloGLViewController.m
+++ HelloGL/HelloGLViewController.m
@@ -8,11 +8,33 @@
 
 #import "HelloGLViewController.h"
 
-@interface HelloGLViewController()
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+const Vertex Vertices[] = {
+    {{1, -1, 0},  {0, 0, 1, 1}},
+    {{1, 1, 0},   {0, 1, 0, 1}},
+    {{-1, 1, 0},  {1, 0, 0, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
+@interface HelloGLViewController() {
+    GLuint vertexBuffer;
+    GLuint indexBuffer;
+}
+
 @property (strong, nonatomic) EAGLContext *context;
 
-@property (nonatomic) float currentRed;
-@property (nonatomic, getter = isIncreasingRed) BOOL increasingRed;
+@property (nonatomic) float rotation;
+
+@property (strong, nonatomic) GLKBaseEffect *effect;
 @end
 
 @implementation HelloGLViewController
@@ -28,27 +50,81 @@
     
     GLKView *view = (GLKView *)self.view;
     [view setContext:self.context];
+    
+    [self setupGL];
+}
+
+- (void)setupGL {
+    [EAGLContext setCurrentContext:self.context];
+    
+    self.effect = [[GLKBaseEffect alloc] init];
+    
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
-    glClearColor(self.currentRed, 0.0, 0.0, 1.0);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    [self.effect prepareToDraw];
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    
+    // enable two pre-defined vertex attributes we want GLKBaseEffect to use, VertexPosition and VertexColor
+    // glVertexAttribPointer feeds the correct values for each attribute
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *) offsetof(Vertex, Position));
+    glEnableVertexAttribArray(GLKVertexAttribColor);
+    glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *) offsetof(Vertex, Color));
+    
+    glDrawElements(GL_TRIANGLES, sizeof(Indices) / sizeof(Indices[0]), GL_UNSIGNED_BYTE, 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 - (void)update {
-    if (self.isIncreasingRed) {
-        self.currentRed += 1.0 * self.timeSinceLastUpdate;
-    } else {
-        self.currentRed -= 1.0 * self.timeSinceLastUpdate;
-    }
+    // get the aspect ratio of the view
+    float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+    // create the projection matrix
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 1.0f, 10.0f);
+    // set the projection matrix
+    self.effect.transform.projectionMatrix = projectionMatrix;
     
-    if (self.currentRed >= 1.0) {
-        self.currentRed = 1.0;
-        self.increasingRed = NO;
-    }
-    if (self.currentRed <= 0.0) {
-        self.currentRed = 0.0;
-        self.increasingRed = YES;
+    // translate modelViewMatrix back 6 units
+    GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -6.0f);
+    // update rotation
+    self.rotation += 90 * self.timeSinceLastUpdate;
+    // rotate modelViewMatrix
+    modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, GLKMathDegreesToRadians(self.rotation), 0.0f, 0.0f, 1.0f);
+    // set the model view matrix
+    self.effect.transform.modelviewMatrix = modelViewMatrix;
+}
+
+- (void)tearDownGL {
+    [EAGLContext setCurrentContext:self.context];
+    
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &indexBuffer);
+    
+    self.effect = nil;
+}
+
+- (void)dealloc {
+    [self tearDownGL];
+
+    if ([EAGLContext currentContext] == self.context) {
+        [EAGLContext setCurrentContext:nil];
     }
 }
 
